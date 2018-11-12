98d0f8a4a97a568ebd03d8ad09ef1d7ac9931de0
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-12 19:55:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,10 @@ function removeElementFromBeginningOfArray (chocolateBars) {
   return chocolateBars;
 }
 
-
+function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
+  chocolateBars.pop()
+  return chocolateBars;
+}
 
 
 
